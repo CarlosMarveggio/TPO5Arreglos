2582d24998da90f141c476f142ef6bd9bc449484
CarlosMarv <carlosmmarveggio@gmail.com>
2023-04-25 14:05:47 -0300

    Initial commit

---
+++ src/tpo5arreglos/TPO5Arreglos.java
@@ -0,0 +1,18 @@
+
+
+package tpo5arreglos;
+
+/**
+ *
+ * @author SE31452
+ */
+public class TPO5Arreglos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logik here
+    }
+
+}
